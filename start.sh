18e07d14aa7a399f29a79b577f0f4e83b7129e6f
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-02-22 15:08:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/4kcinemas/SS_Linkz_Main /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /4Kcinemas
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
